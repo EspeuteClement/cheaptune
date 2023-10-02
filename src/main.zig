8f34b20d95366940f4fcb3421d2a7e5596179339
Valden <espeut.clement@gmail.com>
2023-10-02 20:10:18 +0200

    blep

--- src/main.zig
+++ src/main.zig
@@ -17,6 +17,9 @@ var readback_mutex: std.Thread.Mutex = .{};
 var readback_buffer_copy: [4096][2]f32 = undefined;
 var readback_slice_copy: [][2]f32 = readback_buffer_copy[0..0];
 
+var currentSynth: isize = 0;
+var currentSynthCopy: isize = 0;
+
 const Synth = struct {
     time: f64 = 0.0,
     mutex: std.Thread.Mutex = .{},
@@ -87,16 +90,16 @@ const Synth = struct {
         self.cur_vel = self.cur_vel + (target_vel - self.cur_vel) * 0.1;
 
         for (buffer) |*frame| {
-            var neededHarmonics: usize = @intFromFloat(@floor(nyquist / freq / 2));
+            var neededHarmonics: usize = @intFromFloat(@floor(nyquist / freq));
             neededHarmonics = @min(neededHarmonics, 50);
             var s: f32 = 0.0;
             for (0..std.math.shr(usize, neededHarmonics, 1)) |harmonic| {
                 const fharmonic: f32 = @floatFromInt(harmonic * 2 + 1);
 
-                s += 1.0 / (fharmonic * 2.0 - 1.0) * @sin(@as(f32, @floatCast(self.time * std.math.tau)) * fharmonic);
+                s += 1.0 / (fharmonic) * @sin(@as(f32, @floatCast(self.time * std.math.tau)) * fharmonic);
             }
 
-            s *= 1.0 / std.math.pi;
+            s *= 4.0 / std.math.pi;
 
             s *= self.cur_vel;
             inline for (frame) |*sample| {
@@ -106,6 +109,54 @@ const Synth = struct {
             self.time = std.math.mod(f64, self.time, 1.0) catch unreachable;
         }
     }
+
+    pub inline fn blep(dt: f32, tIn: f32) f32 {
+        var t = tIn;
+        if (t < dt) {
+            t /= dt;
+            return t + t - t * t - 1.0;
+        } else if (t > 1.0 - dt) {
+            t = (t - 1.0) / dt;
+            return t * t + t + t + 1.0;
+        }
+        return 0.0;
+    }
+
+    pub fn renderBlep(self: *Self, buffer: [][numChannels]f32) void {
+        if (self.mutex.tryLock()) {
+            defer self.mutex.unlock();
+            self.cur_note = self.note;
+        }
+        self.prev_note = self.cur_note orelse self.prev_note;
+
+        const A = 440.0;
+        var freq: f32 = (A / 32.0) * std.math.pow(f32, 2.0, @as(f32, @floatFromInt((self.prev_note) - 9)) / 12.0);
+
+        var target_vel: f32 = if (self.cur_note != null) 0.50 else 0.0;
+        self.cur_vel = self.cur_vel + (target_vel - self.cur_vel) * 0.5;
+        const inc: f32 = 1.0 / @as(f32, sampleRate) * freq;
+        for (buffer) |*frame| {
+            var v: f32 = if (self.time < 0.5) 1.0 else -1.0;
+
+            v += blep(inc, @floatCast(self.time));
+            v -= blep(inc, @floatCast(@mod(self.time + 0.5, 1.0)));
+
+            var s = v;
+            s *= self.cur_vel;
+            inline for (frame) |*sample| {
+                sample.* = s;
+            }
+            self.time += @floatCast(inc);
+            self.time = std.math.mod(f64, self.time, 1.0) catch unreachable;
+        }
+    }
+
+    const renderers = [_]struct { name: []const u8, cb: *const fn (*Self, [][2]f32) void }{
+        .{ .name = "PolyBlep", .cb = &renderBlep },
+        .{ .name = "Naive", .cb = &renderNaive },
+        .{ .name = "Sine", .cb = &renderSine },
+        .{ .name = "Band Limited", .cb = &renderBandlimited },
+    };
 
     pub fn playNote(self: *Self, wanted_note: ?u8) void {
         self.mutex.lock();
@@ -122,14 +173,19 @@ fn audioCallback(c_buffer: ?*anyopaque, nb_frames: c_uint) callconv(.C) void {
     var buffer: [*][2]f32 = @ptrCast(@alignCast(c_buffer.?));
     var slice: [][2]f32 = buffer[0..nb_frames];
 
-    synth.render(slice);
+    if (readback_mutex.tryLock()) {
+        defer readback_mutex.unlock();
+        currentSynthCopy = currentSynth;
+    }
+
+    const cb = Synth.renderers[@intCast(currentSynthCopy)].cb;
+    cb(&synth, slice);
 
     {
         readback_mutex.lock();
         defer readback_mutex.unlock();
         if (readback_slice.len + nb_frames < readback_buffer.len) {
             var rb_slice = readback_buffer[readback_slice.len..][0..nb_frames];
-            std.debug.print("AAAAAAAA {d}, {d}\n", .{ rb_slice.len, readback_slice.len });
             for (slice, rb_slice) |frame, *rb_frame| {
                 rb_frame.* = frame;
             }
@@ -266,6 +322,13 @@ pub fn main() anyerror!void {
             }
         }
 
+        var delta: isize = @as(isize, @intFromBool(rl.isKeyPressed(rl.KeyboardKey.key_kp_subtract))) - @as(isize, @intFromBool(rl.isKeyPressed(rl.KeyboardKey.key_kp_add)));
+        if (delta != 0) {
+            readback_mutex.lock();
+            defer readback_mutex.unlock();
+            currentSynth = @mod((currentSynth + delta), Synth.renderers.len);
+        }
+
         // Draw
         //----------------------------------------------------------------------------------
         rl.beginDrawing();
@@ -306,9 +369,9 @@ pub fn main() anyerror!void {
                 var f1 = reslice[i + 1];
                 rl.drawLine(
                     @intFromFloat(start_x + fi),
-                    @intFromFloat(start_y + f0[0] * scale),
+                    @intFromFloat(start_y + std.math.clamp(f0[0], -1.0, 1.0) * scale),
                     @intFromFloat(start_x + fi + 1.0),
-                    @intFromFloat(start_y + f1[0] * scale),
+                    @intFromFloat(start_y + std.math.clamp(f1[0], -1.0, 1.0) * scale),
                     rl.Color.black,
                 );
             }
@@ -336,14 +399,20 @@ pub fn main() anyerror!void {
                     mag = std.math.lerp(0.0, screenHeight / 2.0, mag);
 
                     rl.drawRectangle(
-                        @intCast(i * 2),
+                        @intCast(i),
                         @intFromFloat(start_y),
-                        @intCast(2),
+                        @intCast(1),
                         @intFromFloat(mag),
                         rl.Color.dark_gray,
                     );
                 }
             }
+        }
+
+        {
+            readback_mutex.lock();
+            defer readback_mutex.unlock();
+            rl.drawText(@ptrCast(Synth.renderers[@intCast(currentSynth)].name), 8, 8, 20, rl.Color.dark_gray);
         }
 
         rl.clearBackground(rl.Color.white);
